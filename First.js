6a1337fafc475a389ce3cef6bf20536cff4d5fec
poxx1 <julianlastrama@outlook.com>
2021-01-14 17:50:31 -0300

    First push

---
+++ First.js
@@ -0,0 +1,17 @@
+//Agrego parametros para poder pasarle.
+const path = requiere("path");
+
+//Crear variable:
+let hello = "Hello World";
+//console.log(hello);
+
+//Para usar el 'public' de c#. Transformas en global.
+global.console.log(hello);
+
+
+//#region Dirs
+console.log(_dirname);
+console.log(_filename);
+//#endregion
+
+
